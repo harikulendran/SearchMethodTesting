f05f7cdd2983ea106e457c3bc3b6a34d46b917f1
Hari <hk1n15@ecs.soton.ac.uk>
2017-10-16 08:39:19 +0100

    t

--- Algorithms/Algorithms.cpp
+++ Algorithms/Algorithms.cpp
@@ -7,20 +7,15 @@
 using namespace std;
 
 int main() {
-	//BreadthFirstSearch b{};
-	//b.search();
-
-
-
 	__int64 total = 0;
 	int testno = 1;
 	for (int i = 0; i < testno; i++) {
 		auto start = chrono::steady_clock::now();
-		//DepthFirstSearch d{};
+		DepthFirstSearch d{};
 		//d.search();
-		//d.iterativeSearch(0,25,1);
-		BreadthFirstSearch b{};
-		b.search();
+		d.iterativeSearch(0,25,1);
+		//BreadthFirstSearch b{};
+		//b.search();
 		auto dur = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start);
  
 		total += dur.count();
@@ -29,25 +24,6 @@ int main() {
 	}
 	cout << "Avg time taken: " << (double)total / (double)testno << "ms" << endl;
 	
-
-
-
-	/*{
-		BreadthFirstSearch d{};
-		d.search();
-		int qq;
-		cin >> qq;
-	}*/
-
-	/*BlocksWorldBoard b{};
-	b.move(Direction::LEFT);
-	BlocksWorldBoard b2(b);
-	b2.move(Direction::UP);
-
-	b.print();
-	cout << "ADFASDF" << endl;
-	b2.print();
-	*/
 	//int q;
 	//cin >> q;
 
