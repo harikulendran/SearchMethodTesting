4c0681b3efcdf9390b29c8923717f8eec2de52f9
Hari <hk1n15@ecs.soton.ac.uk>
2017-10-21 15:27:52 +0100

    t

--- Algorithms/Algorithms.cpp
+++ Algorithms/Algorithms.cpp
@@ -9,14 +9,14 @@ using namespace std;
 
 int main() {
 	__int64 total = 0;
-	int testno = 10;
+	int testno = 1000;
 	for (int i = 0; i < testno; i++) {
 		auto start = chrono::steady_clock::now();
-		//DepthFirstSearch d{};
-		//d.search();
+		DepthFirstSearch d{};
+		d.search();
 		//d.iterativeSearch(0,25,1);
-		BreadthFirstSearch b{};
-		b.search();
+		//BreadthFirstSearch b{};
+		//b.search();
 		//AStar a{};
 		//a.search();
 		auto dur = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start);
