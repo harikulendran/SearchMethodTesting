37878a3388ac0b8aa908f7608f068df2fa6711ad
Hari <hk1n15@ecs.soton.ac.uk>
2017-11-22 10:19:21 +0000

    cleanup

--- Algorithms/DepthFirstSearch.cpp
+++ Algorithms/DepthFirstSearch.cpp
@@ -3,47 +3,6 @@
 #include "time.h"
 #include <climits>
 
-/*DepthFirstSearch::DepthFirstSearch(int ir) {
-	srand(time(NULL) + ir);
-}
-
-SearchOutput DepthFirstSearch::search() {
-	return search(INT32_MAX);
-}
-
-SearchOutput DepthFirstSearch::iterativeSearch(int initDepth, int maxDepth, int interval) {
-	for (int i = initDepth; i < maxDepth; i += interval) {
-		search(i);
-		if (complete)
-			break;
-	}
-	return output;
-}
-
-SearchOutput DepthFirstSearch::search(int maxDepth) {
-	//push the root node to start
-	visitedNodes.push(NodeState{0,-1,bwBoard});
-	currentNode = visitedNodes.top();
-
-	while (!visitedNodes.empty() && !currentNode.state.isSolved()) {
-		if (currentNode.depth < maxDepth && !currentNode.expanded) {
-			addRandomAdjacentNode();
-			depth++;
-		}
-		else {
-			visitedNodes.pop();
-			output.nodesExpanded++;
-			if (!visitedNodes.empty())
-				currentNode = visitedNodes.top();
-		}
-	}
-	complete = currentNode.state.isSolved();
-	output.solnDepth = currentNode.depth;
-	output.isOptimal = (output.solnDepth == 14);
-	output.nodesInMemory = visitedNodes.size();
-	output.maxNodesInMemory = output.nodesInMemory;
-	return output;
-}*/
 DepthFirstSearch::DepthFirstSearch(int i) {
 	srand(time(NULL) + i);
 }
--- Algorithms/DepthFirstSearch.h
+++ Algorithms/DepthFirstSearch.h
@@ -14,24 +14,3 @@ protected:
 	void calculateF(NodeState* ns);
 	void expandNode();
 };
-
-/*
-class DepthFirstSearch {
-	public:
-		SearchOutput output{};
-		stack<NodeState> visitedNodes;
-		BlocksWorldBoard bwBoard{};
-		NodeState currentNode;
-		int depth = 1;
-		int nodeIndex = 0;
-		bool complete = false;
-
-	public:
-		DepthFirstSearch(int ir = 0);
-
-	public:
-		SearchOutput search();
-		SearchOutput search(int maxDepth);
-		SearchOutput iterativeSearch(int initDepth, int maxDepth, int interval);
-		void addRandomAdjacentNode();
-};*/
